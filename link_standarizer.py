6c6da699a9f61c9f93bd8a48b0ab7132f1127bdd
pdazero <patricio.anguita.osorio@gmail.com>
2020-07-26 14:29:23 -0400

    Refactor and more tests. Added URL markdown link detection.

--- link_standarizer.py
+++ link_standarizer.py
@@ -7,75 +7,193 @@ def wikilink_to_mdlink(wikilink):
     # compatible markdown ![[]](image%20nice.jpg)
     #
     # The trick here is that with double brackets we get compatibility for both wiki-style linkers (1writer,
-    # obsidian for example) and standard markdown editors such as Typora.
+    # Obsidian for example) and standard markdown editors such as Typora.
     #
     # So, [[linked_file]] and [[linked_file]](linked_file) works on both systems. The only catch is that the rendered
     # view will show links inside single brackets - not big deal for me.
 
+    # First, confirm this is a wikilink
+    var_type = link_type(wikilink)
+
+    if var_type == 'wikilink':
+        wikilink_dictionary = wikilink_split(wikilink)
+        filename = wikilink_dictionary['wikilink']
+
+        # Check if its a link to another md note
+        groups = re.search('.*\..*', filename)
+        if not groups:
+            # Add ".md" to the filename
+            filename += ".md"
+
+        # URL-encode it
+        urlencoded_filename = urllib.parse.quote(filename)
+
+        # Now build the markdown link
+        mdlink = wikilink + "(" + urlencoded_filename + ")"
+
+        return mdlink
+    else:
+        return False
+
+# Split wikilink in it's parts
+def wikilink_split(wikilink):
     # Extract the link
     groups = re.search('\[\[(.*)\]\]', wikilink)
+
     if groups:
-        # Proper wikilink
-        filename = groups.group(1)
+        wikilink_split = {
+            'wikilink': groups.group(1)
+        }
+
+        return wikilink_split
     else:
         # Something went wrong
         return False
 
-    # Check if its a link to another md note
-    groups = re.search('.*\..*', filename)
-    if not groups:
-        # Add ".md" to the filename
-        filename += ".md"
 
-    # URL-encode it
-    urlencoded_filename = urllib.parse.quote(filename)
-    #    urllib.parse(filename)
+# Split markdown links in it's parts, else return False
+def mdlink_split(mdlink):
 
-    # Now build the markdown link
-    mdlink = wikilink + "(" + urlencoded_filename + ")"
+    # First check if it is an internal link or url link
+    mdlink_dictionary = internal_mdlink_split(mdlink)
 
-    return mdlink
+    if mdlink_dictionary:
+        # Now see if it's URL
+        url_split = urllib.parse.urlparse(mdlink_dictionary['Path'])
 
-def mdlink_to_wikilink(mdlink):
-    # If title is not double-bracketed, add another pair of brackets and change title to filename for compatibility
+        if url_split.scheme is not '':
+            mdlink_dictionary['Url'] = mdlink_dictionary['Path'] + mdlink_dictionary['Filename']
+            del mdlink_dictionary['Filename']
+            del mdlink_dictionary['Path']
 
-    # Get parts of the mdlink
-    regex = re.compile(r""""
+        return mdlink_dictionary
+    else:
+        return False
+
+# Split markdown INTERNAL links
+def url_mdlink_split(mdlink):
+    # Regex pattern to split markdown internal links in it's parts
+    regex = re.compile(r"""
         ^           # Line begin anchor
-        (!)?        # 1 Embedded
-        \[(.*)\]    # 2 Title
+        (!)?        # 1 Optional Embedded
+        \[(.*)\]    # 2 Optional Title
         \((.*\/)?   # 3 Optional path
-        (.*)\)      # 4 filename
+        (.*)\)      # 4 Filename
         $           # Line end anchor
         """, re.X)
 
-    # groups = regex.search(mdlink)
-    groups = re.search('^(!)?\[(.*)\]\((.*\/)?(.*)\)$', mdlink)
-    if not groups:
-        # Something went wrong
-        return False
+    search = regex.search(mdlink)
+
+    # If regex pattern matches the input string
+    if search:
+
+        mdlink_dictionary = {
+            'Embedded': search.group(1),
+            'Title': search.group(2),
+            'Path': search.group(3),
+            'Filename': search.group(4)
+        }
+
+        # Clean optional components
+        if not mdlink_dictionary['Embedded']: mdlink_dictionary['Embedded'] = ''
+        if not mdlink_dictionary['Title']: mdlink_dictionary['Title'] = ''
+        if not mdlink_dictionary['Path']: mdlink_dictionary['Path'] = ''
+
+        return mdlink_dictionary
+
+    # mdlink was not detected on the input string
     else:
-        if groups.group(1):
-            embedded = groups.group(1)
+        return False
+
+
+# Split markdown INTERNAL links
+def internal_mdlink_split(mdlink):
+    # Regex pattern to split markdown internal links in it's parts
+    regex = re.compile(r"""
+        ^           # Line begin anchor
+        (!)?        # 1 Optional Embedded
+        \[(.*)\]    # 2 Optional Title
+        \((.*\/)?   # 3 Optional path
+        (.*)\)      # 4 Filename
+        $           # Line end anchor
+        """, re.X)
+
+    search = regex.search(mdlink)
+
+    # If regex pattern matches the input string
+    if search:
+
+        mdlink_dictionary = {
+            'Embedded': search.group(1),
+            'Title': search.group(2),
+            'Path': search.group(3),
+            'Filename': search.group(4)
+        }
+
+        # Clean optional components
+        if not mdlink_dictionary['Embedded']: mdlink_dictionary['Embedded'] = ''
+        if not mdlink_dictionary['Title']: mdlink_dictionary['Title'] = ''
+        if not mdlink_dictionary['Path']: mdlink_dictionary['Path'] = ''
+
+        return mdlink_dictionary
+
+    # mdlink was not detected on the input string
+    else:
+        return False
+
+
+# If title is not double-bracketed, add another pair of brackets and change title to filename for compatibility
+def mdlink_to_wikilink(mdlink):
+
+    # Check this is an internal mdlink
+    var_type = link_type(mdlink)
+
+    if var_type == 'internalmdlink':
+        # Get parts of mdlink
+        mdlink_dictionary = mdlink_split(mdlink)
+
+        if not mdlink_dictionary:
+            # Something went wrong
+            return False
+
         else:
-            embedded = ''
+            # Check if title is already double-bracketed
+            groups = re.search('\[.*\].*', mdlink_dictionary['Title'])
+            if not groups:
+                # Add another pair of brackets
+                mdlink_dictionary['Title'] = "[" + mdlink_dictionary['Title'] + "]"
 
-        title = groups.group(2)
+            wikilink = mdlink_dictionary['Embedded'] + "[" + mdlink_dictionary['Title'] + "]" + "(" + mdlink_dictionary['Path'] + mdlink_dictionary['Filename'] + ")"
 
-        if groups.group(3):
-            path = groups.group(3)
+            return wikilink
+
+    else:
+        return False
+
+
+# Analyze the string and see if it's a link, and which type. Returns either:
+# 'urlmdlink': Markdown url link i.e. [title](url)
+# 'internalmdlink': Markdown internal link ie. [titile](path/to/mdfile.md)
+# 'ahreflink': HTML formatted link i.e. <a href='url'>title</a>
+# 'wikilink': Extended markdown wililink i.e. [[mdfile]] (without .md extension)
+# False: No link detected
+def link_type(string):
+    # First check if it is an internal link or url link
+    mdlink_dictionary = internal_mdlink_split(string)
+
+    if mdlink_dictionary:
+        url_split = urllib.parse.urlparse(mdlink_dictionary['Path'])
+
+        if url_split.scheme is not '':
+            return 'urlmdlink'
         else:
-            path = ''
+            return 'internalmdlink'
 
-        filename = groups.group(4)
+    # Not mdlink. Check if it's wikilink
+    else:
+        wikilink_dictionary = wikilink_split(string)
 
+        if wikilink_dictionary:
+            return 'wikilink'
 
-    # Check if title is already double-bracketed
-    groups = re.search('\[.*\].*', title)
-    if not groups:
-        # Add another pair of brackets
-        title = "[" + title + "]"
-
-    wikilink = embedded + "[" + title + "]" + "(" + path + filename + ")"
-
-    return wikilink
+    return False
--- test_link_standarizer.py
+++ test_link_standarizer.py
@@ -13,10 +13,116 @@ class TestEmbeddedImageWikiLinkToMdlink(unittest.TestCase):
         result = link_standarizer.wikilink_to_mdlink("![[image file.png]]")
         self.assertEqual(result, "![[image file.png]](image%20file.png)")
 
+
+class TestMdlinkSplit(unittest.TestCase):
+    def test_mdlink_split(self):
+        # Embedded Image with title but no path
+        result = link_standarizer.mdlink_split("![optional title](some%20image.png)")
+        self.assertEqual(result, {
+            'Embedded': '!',
+            'Title': 'optional title',
+            'Path': '',
+            'Filename': 'some%20image.png'
+        })
+
+        # Embedded Image with title and path
+        result = link_standarizer.mdlink_split("![optional title](/path/to/some%20image.png)")
+        self.assertEqual(result, {
+            'Embedded': '!',
+            'Title': 'optional title',
+            'Path': '/path/to/',
+            'Filename': 'some%20image.png'
+        })
+
+        # Embedded Image with no title and no path
+        result = link_standarizer.mdlink_split("![](some%20image.png)")
+        self.assertEqual(result, {
+            'Embedded': '!',
+            'Title': '',
+            'Path': '',
+            'Filename': 'some%20image.png'
+        })
+
+        # Embedded Image with no title and path
+        result = link_standarizer.mdlink_split("![](/path/to/some%20image.png)")
+        self.assertEqual(result, {
+            'Embedded': '!',
+            'Title': '',
+            'Path': '/path/to/',
+            'Filename': 'some%20image.png'
+        })
+
+        # 'urlmdlink': Markdown url link i.e. [title](url)
+        result = link_standarizer.mdlink_split("[Somefile link](https://go.to/somefile.html)")
+        self.assertEqual(result, {
+            'Embedded': '',
+            'Title': 'Somefile link',
+            'Url': 'https://go.to/somefile.html',
+        })
+
+        result = link_standarizer.mdlink_split("![Embedded url image](https://go.to/somefile.png)")
+        self.assertEqual(result, {
+            'Embedded': '!',
+            'Title': 'Embedded url image',
+            'Url': 'https://go.to/somefile.png',
+        })
+
+
 class TestEmbeddedImageMdlinkToWikiLink(unittest.TestCase):
     def test_mdlink_to_wikilink(self):
+        # Embedded Image with title but no path
         result = link_standarizer.mdlink_to_wikilink("![optional title](some%20image.png)")
         self.assertEqual(result, "![[optional title]](some%20image.png)")
+
+        # Embedded Image with title and path
+        result = link_standarizer.mdlink_to_wikilink("![optional title](/path/to/some%20image.png)")
+        self.assertEqual(result, "![[optional title]](/path/to/some%20image.png)")
+
+        # Embedded Image with no title and no path
+        result = link_standarizer.mdlink_to_wikilink("![](some%20image.png)")
+        self.assertEqual(result, "![[]](some%20image.png)")
+
+        # Embedded Image with no title and path
+        result = link_standarizer.mdlink_to_wikilink("![](/path/to/some%20image.png)")
+        self.assertEqual(result, "![[]](/path/to/some%20image.png)")
+
+class TestLinkType(unittest.TestCase):
+    def test_link_type(self):
+        # 'urlmdlink': Markdown url link i.e. [title](url)
+        result = link_standarizer.link_type("[Somefile link](https://go.to/somefile.html)")
+        self.assertEqual(result, 'urlmdlink')
+
+        result = link_standarizer.link_type("![Embedded url image](https://go.to/somefile.png)")
+        self.assertEqual(result, 'urlmdlink')
+
+        # 'internalmdlink': Markdown internal link ie. [titile](path/to/mdfile.md)
+        result = link_standarizer.link_type("![](/path/to/some%20image.png)")
+        self.assertEqual(result, 'internalmdlink')
+
+        result = link_standarizer.link_type("![Image Title](/path/to/some%20image.png)")
+        self.assertEqual(result, 'internalmdlink')
+
+        result = link_standarizer.link_type("![MD File](/path/to/MD%20File.md)")
+        self.assertEqual(result, 'internalmdlink')
+
+        result = link_standarizer.link_type("![[MD File]](/path/to/MD%20File.md)")
+        self.assertEqual(result, 'internalmdlink')
+
+        # 'ahreflink': HTML formatted link i.e. <a href='url'>title</a>
+        result = link_standarizer.link_type("![[MD File]](/path/to/MD%20File.md)")
+        self.assertEqual(result, 'internalmdlink')
+
+        # 'wikilink': Extended markdown wililink i.e. [[mdfile]] (without .md extension)
+        result = link_standarizer.link_type("![[image file.png]]")
+        self.assertEqual(result, "wikilink")
+
+        result = link_standarizer.link_type("[[MD File]]")
+        self.assertEqual(result, "wikilink")
+
+        # False: No link detected
+        result = link_standarizer.link_type("This does *not* have any links here")
+        self.assertEqual(result, False)
+
 
 if __name__ == '__main__':
     unittest.main()
